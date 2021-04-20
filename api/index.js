54c90460bb9c6a866cf25917203f2b260a9fd466
Kevin Wu <kevinwu@ucsb.edu>
2021-04-20 09:54:50 -0700

    initial commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) => {
+  res.json({
+    body: 'hello there from 20210420',
+    // query: req.query,
+    // cookies: req.cookies,
+  })
+
+  // res.json({
+  //   body: req.body,
+  //   query: req.query,
+  //   cookies: req.cookies,
+  // })
+}
