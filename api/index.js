f6f91f1a2e860007bb65a638bfcefa9bd0dca63f
Kevin Wu <kevinwu@ucsb.edu>
2021-06-02 14:59:56 -0700

    update to 0602

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   res.json({
-    body: 'hello there from 20210420',
+    body: 'hello there from 20210602',
     // query: req.query,
     // cookies: req.cookies,
   })
