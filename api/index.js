806e6b2b8b4d92cfe7b4326e660f93e89474c404
Kevin Wu <kevinwu@ucsb.edu>
2021-09-09 17:02:30 -0700

    more

--- api/index.js
+++ api/index.js
@@ -1,6 +1,39 @@
 module.exports = (req, res) => {
-  console.log('logging entire request payload')
-  console.log(req)
+  console.log('***** req.baseUrl *****')
+  console.log(req.baseUrl)
+
+  console.log('***** req.body *****')
+  console.log(req.body)
+
+  console.log('***** req.hostname *****')
+  console.log(req.hostname)
+
+  console.log('***** req.ip *****')
+  console.log(req.ip)
+
+  console.log('***** req.method *****')
+  console.log(req.method)
+
+  console.log('***** req.params *****')
+  console.log(req.params)
+
+  console.log('***** req.path *****')
+  console.log(req.path)
+
+  console.log('***** req.protocol *****')
+  console.log(req.protocol)
+
+  console.log('***** req.query *****')
+  console.log(req.query)
+
+  console.log('***** req.protocol *****')
+  console.log(req.protocol)
+
+  console.log('***** req.route *****')
+  console.log(req.route)
+
+  // console.log('logging entire request payload')
+  // console.log(req)
   res.json({
     body: 'hello there from 20210909',
     // query: req.query,
