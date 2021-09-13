0c585980be078637da4dcdeb2aa22d12502270c4
Kevin Wu <kevinwu@ucsb.edu>
2021-09-13 09:15:21 -0700

    update

--- api/system/v1.0/testconnection/index.js
+++ api/system/v1.0/testconnection/index.js
@@ -1,44 +1,6 @@
 module.exports = (req, res) => {
-  // console.log('***** req.baseUrl *****')
-  // console.log(req.baseUrl)
-
-  // console.log('***** req.body *****')
-  // console.log(req.body)
-
-  // console.log('***** req.hostname *****')
-  // console.log(req.hostname)
-
-  // console.log('***** req.ip *****')
-  // console.log(req.ip)
-
-  // console.log('***** req.method *****')
-  // console.log(req.method)
-
-  // console.log('***** req.params *****')
-  // console.log(req.params)
-
-  // console.log('***** req.path *****')
-  // console.log(req.path)
-
-  // console.log('***** req.protocol *****')
-  // console.log(req.protocol)
-
-  // console.log('***** req.query *****')
-  // console.log(req.query)
-
-  // console.log('***** req.protocol *****')
-  // console.log(req.protocol)
-
-  // console.log('***** req.route *****')
-  // console.log(req.route)
-
+  console.log('inside test connection')
   res.json({
     'Test-Connection': 'successful',
   })
-
-  // res.json({
-  //   body: req.body,
-  //   query: req.query,
-  //   cookies: req.cookies,
-  // })
 }
